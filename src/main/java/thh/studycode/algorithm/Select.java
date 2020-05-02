c689e306f1f97d7f67aff1902c5aa2d190dccdd5
casparthh <caspar.thh@gmail.com>
2020-05-02 16:47:22 +0800

    add rest api

--- src/main/java/thh/studycode/algorithm/Select.java
+++ src/main/java/thh/studycode/algorithm/Select.java
@@ -8,7 +8,7 @@ public class Select {
         int[] nums = new int[(int) (Math.random() * 100)];
         int[] nums1 = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = (int)(Math.random()*100);
+            nums[i] = (int) (Math.random() * 100);
             nums1[i] = nums[i];
             System.out.print(nums[i] + "\t");
         }
@@ -19,7 +19,7 @@ public class Select {
         System.out.println("");
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + "\t");
-            if(nums[i] != nums1[i]){
+            if (nums[i] != nums1[i]) {
                 System.out.println(false);
                 return;
             }
@@ -29,28 +29,29 @@ public class Select {
 
     /**
      * 选择排序
-     *
+     * 选择排序是给定位置去找数。选择排序是按顺序比较，找最大值或者最小值，每一轮比较都只需要换一次位置；
+     * <p>
      * 依次找出最大值，并移到最后面
-     *
+     * <p>
      * 时间复杂度，O(n*n)
      * 空间复杂度，O(1)
      * 稳定性：不稳
      */
-    public static void select(int[] nums){
+    public static void select(int[] nums) {
         int maxIndex = 0;
         for (int i = 0; i < nums.length; i++) {
-            maxIndex = nums.length-1-i;
-            for (int j = 0; j < nums.length-i ; j++) {
-                maxIndex = nums[j] > nums[maxIndex] ? j: maxIndex;
+            maxIndex = nums.length - 1 - i;
+            for (int j = 0; j < nums.length - i; j++) {
+                maxIndex = nums[j] > nums[maxIndex] ? j : maxIndex;
             }
 
-            if(maxIndex != nums.length-i-1) {
-                swap(nums, maxIndex, nums.length-i-1);
+            if (maxIndex != nums.length - i - 1) {
+                swap(nums, maxIndex, nums.length - i - 1);
             }
         }
     }
 
-    public static void swap(int[] nums, int res, int des){
+    public static void swap(int[] nums, int res, int des) {
         nums[res] = nums[res] ^ nums[des];
         nums[des] = nums[res] ^ nums[des];
         nums[res] = nums[res] ^ nums[des];
