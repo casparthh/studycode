a985b094528f1b24b026a70392c69f694650f6a2
casparthh <caspar.thh@gmail.com>
2020-05-03 11:06:57 +0800

    add rest api

--- src/main/java/thh/studycode/algorithm/Eor03.java
+++ src/main/java/thh/studycode/algorithm/Eor03.java
@@ -7,24 +7,24 @@ package thh.studycode.algorithm;
 public class Eor03 {
 
     public static void main(String[] args) {
-        int num = (int)(Math.random()*10000);
+        int num = -12;
         String str = Integer.toBinaryString(num);
         int c = 0;
         for (int i = 0; i < str.length(); i++) {
             if(str.charAt(i)=='1') c++;
         }
         System.out.println(Integer.toBinaryString(num));
-        System.out.println(c+"次");
+        System.out.println(count(num));
         System.out.println(count(num) == c);
     }
 
     public static int count(int num) {
         int counter = 0;
         int eor = 0;
-        while (num>0){
+        while (num !=0){
             eor = num & (~num +1);
             counter++;
-            num = num ^ eor;
+            num ^= eor;
         }
         return counter;
     }
